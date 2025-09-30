526571584b82bba6a189863cddf210c7fd87cd06
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-09-30 10:27:45 -0400

    Single Model

---
+++ ModelSingle/CustomDataset.py
@@ -0,0 +1,38 @@
+import torch
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.strategies import DDPStrategy
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer
+import pandas as pd
+import torch
+import torch.nn as nn
+from transformers import AutoModel
+import lightning as pl
+import torch
+import torch.nn.functional as F
+
+class CreativityRankingDataset2(Dataset):
+    def __init__(self, csv_file, tokenizer_name="bert-base-uncased", max_length=128):
+        self.data = pd.read_csv(csv_file, header=0)
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        row = self.data.iloc[idx]
+
+        question = str(row[5])
+        r = str(row[6])
+
+        q_inputs = self.tokenizer(question, truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
+        r_inputs = self.tokenizer(r, truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
+
+        row_label = float(row[8])
+
+        return {
+            "question_input": {k: v.squeeze(0) for k, v in q_inputs.items()},
+            "response_input": {k: v.squeeze(0) for k, v in r_inputs.items()},
+            "label": torch.tensor(row_label, dtype=torch.long)
+        }
---
+++ ModelSingle/PolyEncoder.py
@@ -0,0 +1,86 @@
+import torch
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.strategies import DDPStrategy
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer
+import pandas as pd
+import torch
+import torch.nn as nn
+from transformers import AutoModel
+import lightning as pl
+import torch
+import torch.nn.functional as F
+
+class PolyEncoder(nn.Module):
+    def __init__(self, model_name="bert-base-uncased", poly_m=64):
+        super().__init__()
+        self.encoder = AutoModel.from_pretrained(model_name)
+        self.hidden_size = self.encoder.config.hidden_size
+        self.poly_m = poly_m
+
+        # Learnable poly codes
+        self.poly_codes = nn.Embedding(poly_m, self.hidden_size)
+
+        # Init poly-code indices
+        self.register_buffer("poly_code_ids", torch.arange(poly_m))
+
+    def encode_question(self, input_ids, attention_mask, token_type_ids=None):
+        output = self.encoder(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        token_embeddings = output.last_hidden_state  # [B, L, H]
+
+        poly_codes = self.poly_codes(self.poly_code_ids)  # [M, H]
+        poly_codes = poly_codes.unsqueeze(0).expand(token_embeddings.size(0), -1, -1)  # [B, M, H]
+
+        # Attention: poly codes attend to question tokens
+        attn_scores = torch.matmul(poly_codes, token_embeddings.transpose(1, 2))  # [B, M, L]
+        attn_weights = torch.softmax(attn_scores, dim=-1)  # [B, M, L]
+        attended = torch.bmm(attn_weights, token_embeddings)  # [B, M, H]
+        return attended  # [B, M, H]
+
+    def encode_response(self, input_ids, attention_mask, token_type_ids=None):
+        output = self.encoder(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        return output.last_hidden_state[:, 0, :]  # CLS token: [B, H]
+
+    def forward(self, question_inputs, response_inputs):
+        # Encode question and responses
+        q_vecs = self.encode_question(**question_inputs)  # [B, M, H]
+        r_vec = self.encode_response(**response_inputs)   # [B, H]
+
+        # Attention: response attends to poly codes
+        attn_scores = torch.matmul(r_vec.unsqueeze(1), q_vecs.transpose(1, 2)).squeeze(1)  # [B, M]
+        attn_weights = torch.softmax(attn_scores, dim=-1)  # [B, M]
+        context_vec = torch.bmm(attn_weights.unsqueeze(1), q_vecs).squeeze(1)  # [B, H]
+
+        # Dot-product similarity
+        score = torch.sum(context_vec * r_vec, dim=-1)  # [B]
+        return score
+
+class CreativityRanker2(pl.LightningModule):
+    def __init__(self, model_name="bert-base-uncased", poly_m=64, lr=2e-5):
+        super().__init__()
+        self.model = PolyEncoder(model_name, poly_m)
+        self.lr = lr
+        self.loss_fn = nn.MSELoss()
+
+    def forward(self, batch):
+        q_input = batch['question_input']
+        r_input = batch['response_input']   # fixed variable name
+        score = self.model(q_input, r_input)
+        return score
+
+    def training_step(self, batch, batch_idx):
+        s1 = self.forward(batch)
+        label = batch['label'].float()
+        loss = self.loss_fn(s1, label)   # fixed usage
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        s1 = self.forward(batch)
+        label = batch['label'].float()
+        loss = self.loss_fn(s1, label)
+        self.log("val_loss", loss, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=self.lr)

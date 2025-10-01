d399e1d1a77ec48e2acd0753bb46c779ae6bb909
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-09-30 20:35:56 -0400

    Remove grad accum

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from test import computeCorrelation
 
 def main():
 
-    batch = 64
+    batch = 256
     epochs = 1
     devices = torch.cuda.device_count()
     pl.seed_everything(42)
@@ -63,7 +63,6 @@ def main():
         precision="16",
         logger=wandb_logger,
         log_every_n_steps=50,
-        accumulate_grad_batches=4
     )
     trainer.fit(model, train_loader, val_loader)
 

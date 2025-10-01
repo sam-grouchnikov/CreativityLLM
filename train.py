1363a91e74dab9816273e8a8e30bf1d4235c8c1d
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-10-01 16:51:43 -0400

    New training

--- train.py
+++ train.py
@@ -63,7 +63,7 @@ def main():
         precision="16",
         logger=wandb_logger,
         log_every_n_steps=50,
-        val_check_interval=0.1,
+        val_check_interval=0.25,
     )
     trainer.fit(model, train_loader, val_loader)
 

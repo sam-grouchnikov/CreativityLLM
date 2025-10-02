1adba9fb50f1bdd6e9fb22028625870e57ca05ef
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-10-01 21:20:21 -0400

    MAE loss test

--- ModelSingle/CustomDataset.py
+++ ModelSingle/CustomDataset.py
@@ -13,7 +13,10 @@ import torch.nn.functional as F
 
 class CreativityRankingDataset2(Dataset):
     def __init__(self, csv_file, tokenizer_name="bert-base-uncased", max_length=128):
-        self.data = pd.read_csv(csv_file, header=0)
+        self.data = pd.read_csv(csv_file, header=0).fillna("")
+        self.data["prompt"] = self.data["prompt"].astype(str)
+        self.data["response"] = self.data["response"].astype(str)
+
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
         self.max_length = max_length
 
@@ -23,16 +26,28 @@ class CreativityRankingDataset2(Dataset):
     def __getitem__(self, idx):
         row = self.data.iloc[idx]
 
-        question = str(row["prompt"])
-        r = str(row["response"])
+        question = row["prompt"]
+        response = row["response"]
 
-        q_inputs = self.tokenizer(question, truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
-        r_inputs = self.tokenizer(r, truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
+        q_inputs = self.tokenizer(
+            question,
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_length,
+            return_tensors="pt"
+        )
+        r_inputs = self.tokenizer(
+            response,
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_length,
+            return_tensors="pt"
+        )
 
         row_label = float(row["se"])
 
         return {
             "question_input": {k: v.squeeze(0) for k, v in q_inputs.items()},
             "response_input": {k: v.squeeze(0) for k, v in r_inputs.items()},
-            "label": torch.tensor(row_label, dtype=torch.long)
+            "label": torch.tensor(row_label, dtype=torch.float)
         }

cf18292a642b38f239d61d1999e005f20b2d99dd
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-10-04 23:53:45 -0400

    testing

--- Model/train.py
+++ Model/train.py
@@ -28,7 +28,7 @@ def main():
     epochs = 5
     devices = torch.cuda.device_count()
     pl.seed_everything(42)
-    tokenizer = "bert-base-uncased"
+    tokenizer = "bert-large-uncased"
 
 
 
@@ -44,7 +44,7 @@ def main():
     for param in model.model.encoder.parameters():
         param.requires_grad = False
 
-    for layer in model.model.encoder.encoder.layer[-6:]:
+    for layer in model.model.encoder.encoder.layer[-12:]:
         for param in layer.parameters():
             param.requires_grad = True
 

caa86c3973d8a4c5bc3da3e2155e354e961d2178
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-10-05 20:18:31 -0400

    testing

--- Model/PolyEncoder.py
+++ Model/PolyEncoder.py
@@ -18,6 +18,7 @@ from test import computeCorrelation
 class PolyEncoder(nn.Module):
     def __init__(self, model_name, poly_m=64):
         super().__init__()
+        self.model_name = model_name
         self.encoder = AutoModel.from_pretrained(model_name)
         self.hidden_size = self.encoder.config.hidden_size
         self.poly_m = poly_m
@@ -80,9 +81,13 @@ class PolyEncoder(nn.Module):
 
         return score.squeeze(-1)  # [B]
 
+    def getName(self):
+        return self.model_name
+
 class CreativityScorer(pl.LightningModule):
     def __init__(self, model_name, poly_m=64, lr=3e-5):
         super().__init__()
+        self.model_name = model_name
         self.model = PolyEncoder(model_name, poly_m)
         self.lr = lr
 
@@ -124,7 +129,7 @@ class CreativityScorer(pl.LightningModule):
         self.val_preds = []
         self.val_labels = []
 
-        testCorr = computeCorrelation(self, "/home/sam/datasets/TestData.csv", "microsoft/deberta-v3-large", 2, 128)
+        testCorr = computeCorrelation(self, "/home/sam/datasets/TestData.csv", self.model_name, 2, 128)
         self.log("test_corr", testCorr, prog_bar=True)
 
     def configure_optimizers(self):

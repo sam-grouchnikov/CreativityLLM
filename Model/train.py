8df81a8307b7b182c2ed0ee9195be46ae5b3ea44
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-10-13 11:00:44 -0400

    new test data

--- Model/train.py
+++ Model/train.py
@@ -49,7 +49,7 @@ def main():
     for param in model.model.encoder.parameters():
         param.requires_grad = False
 
-    for layer in model.model.encoder.encoder.layer[-18:]:
+    for layer in model.model.encoder.encoder.layer[-32:]:
         for param in layer.parameters():
             param.requires_grad = True
 

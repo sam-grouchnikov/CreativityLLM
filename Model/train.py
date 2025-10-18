8a0339995192191dcb659e6cd746e62a7b0bb3af
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-10-18 14:37:14 -0400

    m=64

--- Model/train.py
+++ Model/train.py
@@ -25,7 +25,7 @@ def main():
     # train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
     train_loader = DataLoader(trainDataset, batch_size=batch, shuffle=True, num_workers=15)
     val_loader = DataLoader(valDataset, batch_size=batch, shuffle=False, num_workers=15)
-    wandb_logger = WandbLogger(project="poly-m-comps", name="db-l-256")
+    wandb_logger = WandbLogger(project="poly-m-comps", name="db-l-64")
 
     # checkpoint_callback = ModelCheckpoint(
     #     monitor='val_pearson',

301d342d3e0b660c989684cebee6bf4329f04646
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-10-23 21:33:17 -0400

    random testing

--- Model/PolyEncoder.py
+++ Model/PolyEncoder.py
@@ -104,7 +104,7 @@ class PolyEncoder(nn.Module):
         return self.model_name
 
 class CreativityScorer(pl.LightningModule):
-    def __init__(self, model_name, logger, poly_m=512, lr=1e-7):
+    def __init__(self, model_name, logger, poly_m=512, lr=1e-3):
         super().__init__()
         self.model_name = model_name
         self.model = PolyEncoder(model_name, poly_m)
